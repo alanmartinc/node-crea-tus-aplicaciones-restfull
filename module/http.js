4fd1233754c01ecd543ca0f0b5d2784727c3fbf7
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-21 18:23:59 -0300

    modulo HTTP built-in de nodeJS continuación

--- module/http.js
+++ module/http.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+/*
 const server = http.createServer();
 
 server.on('connection', (socket) => {
@@ -8,3 +9,20 @@ server.on('connection', (socket) => {
 
 server.listen(2012);
 console.log('Escuchando en el puerto 2012...');
+*/
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Hola Mundo');
+        res.write('desde NodeJS');
+        res.end();
+    }
+
+    if(req.url == '/coches') {
+        res.write('Coche1');
+        res.end();
+    }
+});
+
+server.listen(3030);
+console.log('Escuchando en el puerto 3030...')
