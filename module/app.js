136cbd06fcd1117745185c844fc298fbf2bcc4a9
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-21 16:38:53 -0300

    añadiendo funciones a nuestro modulo

--- module/app.js
+++ module/app.js
@@ -1,3 +1,7 @@
 var sumar = require('./sumar.js');
 
+// Opción 1
 console.log(sumar.sumarDosMasDos());
+
+// Opción 2
+console.log(sumar.suma(5, 5));
--- module/sumar.js
+++ module/sumar.js
@@ -1,5 +1,10 @@
+// Opción 1
 var sumarDosMasDos = function() {
     return 2 + 2;
 }
-
 exports.sumarDosMasDos = sumarDosMasDos;
+
+// Opción 2
+exports.suma = function(numero1, numero2) {
+    return numero1 + numero2;
+}
