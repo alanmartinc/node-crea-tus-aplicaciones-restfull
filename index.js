bd7580a2a6da9ea882041c5f9278546dafd66b66
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-22 02:43:30 -0300

    fin refactorizacion

--- index.js
+++ index.js
@@ -6,23 +6,5 @@ app.use(express.json());
 app.use('/api/cars/', car);
 
 const port = process.env.PORT || 3003;
-const date = require('./module/date');
-const morgan = require('morgan');
-
-// Middleware de Express
-app.use(morgan('tiny'));
-
-// Middleware de un Module
-app.use(date);
-
-// Middleware Anidado
-app.use('/api/cars/list', function(req, res, next) {
-    console.log('Request Type: ', req.method);
-    next();
-});
-
-app.get('/', function(req, res) {
-    res.send('Hello World');
-});
 
 app.listen(port, () => console.log('Escuchando puerto: ' + port));
