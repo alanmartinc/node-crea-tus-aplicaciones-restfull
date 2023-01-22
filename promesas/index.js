7092862816db901129bdc0096ed75ce02c1045f6
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-22 12:30:33 -0300

    consumir promesas

--- promesas/index.js
+++ promesas/index.js
@@ -1,6 +1,10 @@
-new Promise((resolve, reject) => {
+const promesa = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve({id: 1, company: "Seat", model: "Leon"});
-        reject(new Error('Se ha producido un error al leer la DB'));
+        // reject(new Error('Se ha producido un error al leer la DB'));
     }, 4000)
 });
+
+promesa
+    .then(result => console.log(result))
+    .catch(err => console.log(err.message))
