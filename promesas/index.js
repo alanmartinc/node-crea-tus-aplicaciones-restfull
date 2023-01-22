1f7c52b97fbda149454fe28f3dca92c4385f86d4
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-22 15:47:48 -0300

    promesas anidadas

--- promesas/index.js
+++ promesas/index.js
@@ -1,10 +1,40 @@
+// PROMESAS
+/*
 const promesa = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve({id: 1, company: "Seat", model: "Leon"});
+        resolve({id: 1, company: 'Seat', model: 'Leon'});
         // reject(new Error('Se ha producido un error al leer la DB'));
     }, 4000)
 });
 
 promesa
     .then(result => console.log(result))
+    .catch(err => console.log(err.message))
+*/
+
+// PROMESAS ANIDADAS
+function getCar(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Obtenido coche 23 de nuestra base de datos');
+            resolve({id: 23, company: 'BMW', model: 'X3'});
+        }, 3000)
+    });
+}
+
+function getModel(model) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Obtenido modelo X3 de BMW');
+            resolve({speed: 200, seat: 5, size: '4*5'});
+        }, 3000)
+    });
+}
+
+const promesa = getCar(23);
+promesa.then(coche => console.log(coche));
+
+promesa
+    .then(coche => getModel(coche.model))
+    .then(model => console.log(model))
     .catch(err => console.log(err.message))
